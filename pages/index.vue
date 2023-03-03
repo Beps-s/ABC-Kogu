2e4d879813ac5ed172c8e75cb646828931d15192
ArnoldITA20 <arnold.grosberg@voco.ee>
2023-03-03 15:03:30 +0200

    Create open filters button in the main page and close filterModal functionality. Filtering items is still WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <filterModal/>
+  <div v-show="!showFilterModal">
+  <button @click="showFilterModal = true" style="cursor: pointer;" class="button">Ava filtrid</button>
+  </div>
+  <filterModal v-show="showFilterModal" @close-modal="showFilterModal = false" @filter="filter" />
   <div class="container">
     <div class="row row-cols-4">
       <div v-for="b in books">
@@ -11,8 +14,26 @@
 
 <script setup>
 const {data: books} = await useFetch('http://192.168.31.24:5000/books')
+console.log(books._value)
+</script>
+<script>
+import filterModal from '~/components/filterModal.vue'
+
+export default {
+  components: {filterModal },
+  data() {
+    return {
+      showFilterModal: true,
+    }
+  },
+  methods: {
+    filter(tag) {
+      console.log(tag)
+    }
+  }
+}
+
 </script>
 
 <style scoped>
-
 </style>
